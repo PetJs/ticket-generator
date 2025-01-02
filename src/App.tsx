ba3549ae9c2e021bf629ac7aefccbd3c76c11328
PetJs <peterfagoroye76@gmail.com>
2025-01-02 18:20:12 +0100

    fixed scrolling issue

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,12 @@ function App() {
     setFiles((prevFiles) => [
       ...prevFiles, 
       ...acceptedFiles.map(file => 
-          Object.assign(file, { preview: URL.createObjectURL(file)})
-        ),
+          Object.assign(file, { preview: URL.createObjectURL(file)}),
+      )
       ]
     );
 
-    if(fileRejections.length > 0){
+    if (fileRejections.length > 0) {
       setError("File too large. Please upload a photo under 500kb")
     }
   }, []);
@@ -36,7 +36,7 @@ function App() {
     };
   }, [files]);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: {
       'image/jpeg': [],
@@ -54,51 +54,58 @@ function App() {
   };
 
   return (
-    <div className="relative h-screen bg-[url('./assets/images/background-desktop.png')] bg-cover bg-center overflow-hidden">
-      <div>
-        <img src={patternCircle} alt="" className="relative md:w-32 w-20 md:ml-6 md:-top-12 -top-6 md:left-0 -left-3" />
-        <img src={patterLine} className="absolute inset-0" />
-        <img src={patternTop} className="absolute top-6 right-0 mt md:w-80 w-28 " />
-        <img src={patternCircle} alt="" className="absolute md:right-32 -right-10 md:w-32 w-20 md:mt-64 mt-36 " />
-        <img src={patternBottom} alt="" className="absolute bottom-0 w-96 " />
+    <div className="relative w-full h-screen bg-[url('./assets/images/background-desktop.png')] bg-cover bg-center overflow-auto">
+      <div className="">
+        <img src={patternCircle} alt="" className="fixed md:w-32 w-20 md:ml-6 md:-top-12 -top-6 md:left-0 -left-3" />
+        <img src={patterLine} className="fixed inset-0" />
+        <img src={patternTop} className="fixed top-6 right-0 mt md:w-80 w-28 " />
+        <img src={patternCircle} alt="" className="fixed md:right-32 -right-10 md:w-32 w-20 md:mt-64 mt-36 " />
+        <img src={patternBottom} alt="" className="fixed bottom-0 w-96" />
       </div>
-      <div className="flex text-white absolute top-0 left-1/2 transform -translate-x-1/2 mt-8 gap-4">
-        <img src={Logo} alt="" />
-        <h2 className="font-inconsolata font-semibold md:text-xl text-lg">Coding Conf</h2>
-      </div>
-      <div className="text-white flex flex-col justify-center items-center font-inconsolata m-4">
-        <h1 className="md:text-5xl text-2xl max-w-2xl mx-auto text-center font-semibold">
-          Your Journey to Coding Conf 2025 Starts Here!
-        </h1>
-        <p className="max-w-2xl mx-auto text-center">
-          Secure your spot at next year's biggest coding conference
-        </p>
-      </div>
-      <form onSubmit={handleSubmit} className="text-white relative border flex justify-center w-80 absolute left-1/2 transform -translate-x-1/2 p-3">
-        <div className="w-full">
-          <p>Upload Avatar</p>
-          <div {...getRootProps()} className="cursor-pointer bg-gray-700 opacity-50 rounded border-dashed border-2 p-2">
-            <input {...getInputProps()} />
-            {isDragActive ? (
-              <p>Drop the files here ...</p>
-            ) : (
-              <div className="flex flex-col items-center">
-                <img src={iconUpload} alt="" className="w-12"/>
-                <p>Drag and drop or click to upload</p>
-              </div>
-            )}
-          </div>
-          <ul>
-            {files.map((file) => (
-              <li key={file.name}><img src={file.preview} alt="" /></li>
-            ))}
-          </ul>
-          <div>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet neque voluptas placeat consequatur cum quisquam blanditiis unde totam! Aliquam quisquam natus voluptatum mollitia doloremque! Distinctio aspernatur a illum nesciunt nemo?
-          </div>
+      <div className="relative">
+        <div className="text-white flex gap-4 justify-center items-center font-inconsolata m-4">
+          <img src={Logo} alt="" />
+          <h2 className="font-inconsolata font-semibold md:text-xl text-lg">Coding Conf</h2>
         </div>
-        
-      </form>
+        <div className="text-white flex flex-col justify-center items-center font-inconsolata m-4">
+          <h1 className="md:text-5xl text-2xl max-w-2xl mx-auto text-center font-semibold">
+            Your Journey to Coding Conf 2025 Starts Here!
+          </h1>
+          <p className="max-w-2xl mx-auto text-center">
+            Secure your spot at next year's biggest coding conference
+          </p>
+        </div>
+
+        {/* Form with overflow handling */}
+        <form onSubmit={handleSubmit} className="text-white border flex flex-col justify-center w-80 md:w-96 absolute left-1/2 transform -translate-x-1/2 p-3  ">
+          <div className="w-full">
+            <p>Upload Avatar</p>
+            <div {...getRootProps()} className="cursor-pointer bg-gray-700 opacity-50 rounded border-dashed border-2 p-2">
+              <input {...getInputProps()} />
+              {isDragActive ? (
+                <p>Drop the files here ...</p>
+              ) : (
+                <div className="flex flex-col items-center">
+                  <img src={iconUpload} alt="" className="w-12"/>
+                  <p>Drag and drop or click to upload</p>
+                </div>
+              )}
+            </div>
+            <ul>
+              {files.map((file) => (
+                <li key={file.name}><img src={file.preview} alt="" /></li>
+              ))}
+            </ul>
+            {/* Content that may overflow */}
+            <div className="overflow-auto">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet neque voluptas placeat consequatur cum quisquam blanditiis unde totam! Aliquam quisquam natus voluptatum mollitia doloremque! Distinctio aspernatur a illum nesciunt nemo?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, ullam numquam. Deleniti beatae velit architecto! Provident nemo eaque error necessitatibus cumque tempora. Accusamus harum deserunt, minima facere earum ad sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur libero distinctio deleniti eveniet dolorum officiis molestias vel recusandae illum doloremque. Ipsum, dolorem error repudiandae ducimus veritatis impedit beatae quibusdam nulla.
+            </div>
+          </div>
+        </form>
+      </div>
+      
     </div>
   );
 }
